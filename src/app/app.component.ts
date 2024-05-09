2e55d94f15e8ecc5aa772cb91dcb6f8632ea041d
Duda Gomes <88250544+duudaa1@users.noreply.github.com>
2024-05-08 23:21:36 -0300

    feat: cards and chips initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
+import { ChipsComponent } from './components/chips/chips.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent, ChipsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
