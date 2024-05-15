8fb2f76b03090f138ef5c6a37f663b0359339402
Marllonv <marllonvicente20@gmail.com>
2024-05-15 18:26:06 -0300

    Add Nav-Bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,17 @@ import { CardComponent } from './components/card/card.component';
 import { ChipsComponent } from './components/chips/chips.component';
 import { ListComponent } from './components/list/list.component';
 import { TableComponent } from './components/table/table.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent, ChipsComponent, ListComponent, TableComponent],
+  imports: [RouterOutlet,
+            CardComponent,
+            ChipsComponent, 
+            ListComponent, 
+            TableComponent, 
+            NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
