73bb37b5c6d89a828ebac4df252477cda384df0d
matheus <matheus125gui@gmail.com>
2024-05-17 09:31:37 -0300

    Criação do componente Chip

--- src/app/components/chips/chips.component.ts
+++ src/app/components/chips/chips.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import {MatChipsModule} from '@angular/material/chips';
+
 
 @Component({
   selector: 'app-chips',
   standalone: true,
-  imports: [MatChipsModule],
+  imports: [MatChipsModule, CommonModule],
   templateUrl: './chips.component.html',
   styleUrl: './chips.component.css'
 })
 export class ChipsComponent {
-
+  @Input() text: string = "Static Chips";
+  @Input('bg-color') bgColor: string = "#dce0dd ";
 }
