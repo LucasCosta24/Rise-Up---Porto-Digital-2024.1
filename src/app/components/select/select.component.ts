35cac6cdc41f19751bcafdef46a89ee53f653013
D.JGV <jvincentino@gmail.com>
2024-05-16 21:39:40 -0300

    feat button and select

---
+++ src/app/components/select/select.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+/** @title Select with custom trigger text */
+@Component({
+  selector: 'select-custom-trigger-example',
+  templateUrl: 'select.component.html',
+  styleUrl: 'select.component.css',
+  standalone: true,
+  imports: [MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule],
+})
+export class SelectComponent {
+  Nomes = new FormControl('');
+
+  toppingList: string[] = ['Marllon', 'João', 'Luquinhas', 'Duda', 'José', 'Matheus','Joninhas'];
+}
