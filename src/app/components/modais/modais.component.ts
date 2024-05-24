bd8994f32663ba849c88b5fb1f6873a870beec85
DaniloSantos31 <dandan.santos3103@gmail.com>
2024-05-24 11:34:13 -0300

    mudanças no componente modal

---
+++ src/app/components/modais/modais.component.ts
@@ -0,0 +1,14 @@
+import { Component, Inject } from '@angular/core';
+
+@Component({
+  selector: 'app-modais',
+  standalone: true,
+  imports: [],
+  templateUrl: './modais.component.html',
+  styleUrl: './modais.component.css'
+})
+export class ModaisComponent {
+
+}
+
+
