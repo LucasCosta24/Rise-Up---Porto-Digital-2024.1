ef21c72d895076de139d49e456a083b1f1eeed59
DaniloSantos31 <dandan.santos3103@gmail.com>
2024-05-24 11:33:54 -0300

    mudanças no componente chip

--- src/app/components/chips/chips.component.ts
+++ src/app/components/chips/chips.component.ts
@@ -8,16 +8,40 @@ import {MatChipsModule} from '@angular/material/chips';
   standalone: true,
   imports: [MatChipsModule, CommonModule],
   templateUrl: './chips.component.html',
-  styleUrl: './chips.component.css',
-  
-
+  styleUrl: './chips.component.css'
 })
 export class ChipsComponent {
   @Input() text1: string = "Chip um";
   @Input('bg-color') bgColor: string = "#dce0dd ";
   @Input() text2: string = "Chip dois";
   @Input('bg-color') bgColor1: string = "#839fc7";
-}
+
+
+  @ViewChild('chip') chipRef: ElementRef|undefined;
+
+  handleClick() {
+    const newName = prompt('Digite um novo nome para o chip:');
+    if (newName !== null && newName.trim() !== '') {
+      const limiteCaracteres = 10;
+
+      if(newName.trim().length > limiteCaracteres){
+        alert(`Por favor, digite no máximo ${limiteCaracteres} caracteres.`);
+        return; // Saia da função sem fazer nada
+      }
+      
+      if (newName.trim() !== '') {
+        console.log(this.chipRef?.nativeElement);
+        if (this.chipRef) {
+          this.chipRef.nativeElement.innerHTML = newName;
+        }
+        
+      }
+      }
+      console.log(this.chipRef!.nativeElement); 
+      this.chipRef!.nativeElement.innerHTML = newName;
+    }
+    
+  }
 
 
 
