5b7964433b7d9daa61f4b1efca8316e20cf471f4
Jonatas10J <lopesjonatas10@gmail.com>
2024-05-29 20:32:53 -0300

    pull

--- src/app/components/modais/modais.component.spec.ts
+++ src/app/components/modais/modais.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModaisComponent } from './modais.component';
+// src/main.ts
+import { bootstrapApplication } from '@angular/platform-browser';
+
+
+bootstrapApplication(ModaisComponent)
+  .catch(err => console.error(err));
 
 describe('ModaisComponent', () => {
   let component: ModaisComponent;
