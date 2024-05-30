3c4b6d5f70880778efcdc78cb2012703e1fe1db9
Marllonv <marllonvicente20@gmail.com>
2024-05-30 15:27:13 -0300

    Default Page Update

--- src/app/pages/default-page/default-page.component.ts
+++ src/app/pages/default-page/default-page.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { ListComponent } from '../../components/list/list.component';
+import { Highlight, HighlightAuto } from 'ngx-highlightjs';
+import { HighlightLineNumbers } from 'ngx-highlightjs/line-numbers';
 
 @Component({
   selector: 'app-default-page',
   standalone: true,
-  imports: [],
+  imports: [ListComponent, Highlight, HighlightAuto , HighlightLineNumbers],
   templateUrl: './default-page.component.html',
   styleUrl: './default-page.component.css'
 })
 export class DefaultPageComponent {
-
+  codeForHTML: string = "<app-list type = 'simples'></app-list>"
 }
